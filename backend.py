ee02d83ddac93d2526304795dbeb44ad670e534b
Ron Balannik <ron4ron@gmail.com>
2019-11-06 13:46:05 +0200

    More stable and smarter card chooser with less errors

--- backend.py
+++ backend.py
@@ -3,69 +3,110 @@ import re
 import sys
 import json
 
-
-TAKI_ACTIVE = False
-prev_card = None
+# --- CONSTANTS ---
 json_kwargs = {'default': lambda o: o.__dict__, 'sort_keys': True, 'indent': 4}
 HOST = 'localhost'
 PORT = 50000
 BUFSIZ = 4096
 ADDR = (HOST, PORT)
+# ----------------
+prev_info_recv = None  # Previous information taken from server (for comp. with new info)
+taki_active = False  # If Taki has been played but not yet closed
 
 
-def choose_card(info_recv):
-    global TAKI_ACTIVE
-    global prev_card
+def try_to_place(info_recv, card, order=""):
+    global taki_active
+    if taki_active:
+        additional_cards = []
+        for crd in info_recv['hand']:
+            if crd['color'] == info_recv['pile_color'] and crd != card:
+                additional_cards.append(crd)
+        if len(additional_cards) == 0 or card['value'] == 'STOP' or card['value'] == 'CHDIR':
+            taki_active = False
+            return {'card': card, 'order': 'close_taki'}
+    if card != '':
+        return {'card': card, 'order': order}
+    else:
+        return {'card': {'value': '', 'color': ''}, 'order': order}
+
+
+def choose_card(info_recv, prev_info_recv):
+    global taki_active
     hand = info_recv['hand']
     pile = info_recv['pile']
+    # Pile Color is needed because sometimes the color is unknown just looking at the top card
     pile_color = info_recv['pile_color']
-    card_options = []
+    direction = info_recv['turn_dir']
+    other_hands = info_recv['others']
+    options = []
+    # This block of code checks if a +2 needs to be added
+    if prev_info_recv:
+        other_hands_prev = prev_info_recv['others']
+        if len(other_hands) == len(other_hands_prev):
+            need_to_plus2 = True
+            for i in range(len(other_hands)):
+                if other_hands_prev[i] + 2 <= other_hands[i]:
+                    need_to_plus2 = False
+            if need_to_plus2 and pile['value'] == '+2':
+                for c in hand:
+                    if c['value'] == '+2' and c['color'] == pile_color:
+                        return try_to_place(info_recv, c)
+                return try_to_place(info_recv, "", 'draw card')
+
     for c in hand:
-        if pile['value'] != '+2' or prev_card['value'] != '+2':
-            if c['color'] == pile_color or c['value'] == pile['value'] or c['color'] == 'ALL':
-                card_options.append(c)
-        else:
-            if c['value'] == pile['value']:
-                card_options.append(c)
-    if len(card_options) == 0:
-        return {'card': {"color": "", "value": ""}, 'order': 'draw card'}
-    else:
-        card_being_played = card_options[0]
-        if card_being_played['value'] == 'CHCOL':
-            return {'card': card_being_played, 'order': 'red'}
-        if card_being_played['value'] == 'TAKI':
-            TAKI_ACTIVE = True
-        if TAKI_ACTIVE and len(card_options) == 1:
-            TAKI_ACTIVE = False
-            return {'card': card_being_played, 'order': 'close taki'}
-        return {'card': card_being_played, 'order': ''}
+        if c['value'] == 'TAKI' and (c['color'] == pile_color or c['color'] == 'ALL'):
+            taki_active = True
+            return try_to_place(info_recv, c)
+    for c in hand:
+        if c['value'] == '+' and c['color'] == pile_color:
+            return try_to_place(info_recv, c)
+    for c in hand:
+        if (c['value'] == 'STOP' or c['value'] == 'CHDIR') and c['color'] == pile_color:
+            return try_to_place(info_recv, c)
+    for c in hand:
+        if (9 <= c['value'] >= 0) and c['color'] == pile_color:
+            return try_to_place(info_recv, c)
+    for c in hand:
+        if c['value'] == pile['value']:
+            return try_to_place(info_recv, c)
+    for c in hand:
+        if c['value'] == '+2' and c['color'] == pile_color:
+            return try_to_place(info_recv, c)
+    for c in hand:
+        if c['value'] == 'CHCOL':
+            colors = {'red': 0, 'yellow': 0, 'green': 0, 'blue': 0}
+            for crd in hand:
+                try:
+                    colors[crd['color']] += 1
+                except:
+                    continue
+            return try_to_place(info_recv, c, order=max(colors, key=colors.get))
+    return try_to_place(info_recv, "", 'draw card')
 
 
-tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock = socket(AF_INET, SOCK_STREAM)  # Initial connection to server
 tcpCliSock.connect(ADDR)
-tcpCliSock.send('1234')
+tcpCliSock.send('1234')  # Server Password
 info_recv = tcpCliSock.recv(BUFSIZ)
 print('connected')
-info_recv = tcpCliSock.recv(BUFSIZ)
-our_id = int(re.findall('[0-9]+', info_recv[4:])[0])
+info_recv = tcpCliSock.recv(BUFSIZ)[4:]
+our_id = int(re.findall('[0-9]+', info_recv)[0])  # Retrieval of our ID from the server
 print(our_id)
 if our_id > 5:
     our_id = 3
 while True:
-    info_recv =tcpCliSock.recv(BUFSIZ)[4:]
-    if 'Over' in info_recv:
-        sys.exit()
+    info_recv = tcpCliSock.recv(BUFSIZ)[4:]
     try:
         info_recv = json.loads(info_recv)
     except:
-        print(info_recv)
+        print("JSON NOT RECIEVED:")
         print(info_recv)
         sys.exit()
     turn = info_recv['turn']
     if turn == our_id:
         print(info_recv)
-        command = choose_card(info_recv)
+        command = choose_card(info_recv, prev_info_recv)  # Choose which card to play
         print(command)
         command = json.dumps(command, **json_kwargs)
         tcpCliSock.send(command)
-    prev_amount = info_recv['pile']
+    prev_info_recv = info_recv
